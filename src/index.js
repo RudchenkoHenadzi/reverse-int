394ed3f35bd8ad46230c4e62ebd29d926330bc35
Henadzi <RudchenkoHenadzi@users.noreply.github.com>
2021-03-14 01:22:24 +0300

    feat: result task: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+
+    let posN;
+    let posNrev;
+    if(n < 0) {
+        posN = Math.abs(n);
+        posNrev = String(posN).split('').reverse().join('');
+        return +posNrev;
+    } else {
+        let nSplit = String(n).split('').reverse().join('');
+        return +nSplit;
+    }
+
 }
