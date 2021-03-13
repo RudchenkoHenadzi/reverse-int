c190a7b7ba8c63b88b98a3096e1468cbfe3681f3
Henadzi <RudchenkoHenadzi@users.noreply.github.com>
2021-03-14 01:29:00 +0300

    refactor: improved the code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 module.exports = function reverse (n) {
 
-    let posN;
-    let posNrev;
     if(n < 0) {
-        posN = Math.abs(n);
-        posNrev = String(posN).split('').reverse().join('');
+        let posN = Math.abs(n);
+        let posNrev = String(posN).split('').reverse().join('');
         return +posNrev;
     } else {
         let nSplit = String(n).split('').reverse().join('');
